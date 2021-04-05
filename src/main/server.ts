45e47035add9f3563f30537b01151c86dc14e8c0
Elian Batista <elianbatista2000@gmail.com>
2021-04-05 18:01:33 -0300

    chore: config server and routes

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,17 @@
 import 'module-alias/register'
 import env from './config/env'
 
-const app = (await import('./config/app')).default
-app.listen(env.port, () => console.log(`
-  ####################################################
-  ## Server running at http://localhost:${env.port} ##
-  ####################################################
-`))
+const initServer = async (): Promise<void> => {
+  const app = (await import('./config/app')).default
+  app.listen(env.port)
+}
+
+initServer().then(() => {
+  console.log(`
+    ####################################################
+    ## Server running at http://localhost:${env.port} ##
+    ####################################################
+  `)
+}).catch((error) => {
+  console.error(error)
+})
