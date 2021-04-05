2deec6c25a6435c98c82ec09593d659d961ec1d6
Elian Batista <elianbatista2000@gmail.com>
2021-04-05 16:36:16 -0300

    chore: restrict template expressions

---
+++ src/main/server.ts
@@ -0,0 +1,9 @@
+import 'module-alias/register'
+import env from './config/env'
+
+const app = (await import('./config/app')).default
+app.listen(env.port, () => console.log(`
+  ####################################################
+  ## Server running at http://localhost:${env.port} ##
+  ####################################################
+`))
