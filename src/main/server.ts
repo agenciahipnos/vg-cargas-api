f30bdbbb4fc43a5a3803ec2faab02af0bc4b1c3a
Elian Batista <elianbatista2000@gmail.com>
2021-04-06 20:06:37 -0300

    chore: configure the connection to mongodb

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,16 +1,34 @@
+import mongoose from 'mongoose'
 import 'module-alias/register'
 import env from './config/env'
+import { User } from '@/infra/db/mongodb/schemas/user-schema'
 
 const initServer = async (): Promise<void> => {
   const app = (await import('./config/app')).default
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  mongoose.connect(
+    'mongodb://localhost:27017/vg-cargas', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
   app.listen(env.port)
 }
 
-initServer().then(() => {
+initServer().then(async () => {
+  await User.create({
+    email: 'teste',
+    password: 'teste',
+    name: 'teste',
+    cpf: 'teste',
+    birthdate: 'teste',
+    phone: 'teste',
+    type: 'teste'
+  })
   console.log(`
-    ####################################################
-    ## Server running at http://localhost:${env.port} ##
-    ####################################################
+    #####################################################
+    ##     Server running at http://localhost:${env.port}     ##
+    #####################################################
   `)
 }).catch((error) => {
   console.error(error)
