942b26f70626efc8013e4885e592d5d47899e9c8
Elian Batista <elianbatista2000@gmail.com>
2021-04-05 22:08:41 -0300

    feat: freight entitie

---
+++ src/domain/entities/freight.ts
@@ -0,0 +1,13 @@
+export interface Freight {
+  origin_city: string
+  origin_state: string
+  formated_price: string
+  price: number
+  formated_fee: string
+  fee: number
+  vehicle_type: string
+  bodywork: string
+  load_type: string
+  weight: number
+  comments: string
+}
