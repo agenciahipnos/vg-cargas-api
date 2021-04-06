cebb877a7c61a0f92942a14e4b74670f7ce5a7ea
Elian Batista <elianbatista2000@gmail.com>
2021-04-05 22:15:59 -0300

    feat: create the vehicle model

---
+++ src/domain/models/vehicle-model.ts
@@ -0,0 +1,13 @@
+export interface VehicleModel {
+  id: string
+  modelo: string
+  marca: string
+  ano: string
+  capacidade_maxima: string
+  categoria_veiculo: string
+  placa: string
+  carroceria: string
+  antt: string
+  seguro: boolean
+  rastreador: boolean
+}
