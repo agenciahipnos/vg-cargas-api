ce4e54405f7e3f69a8f40c166b6c3534d628fa59
Elian Batista <elianbatista2000@gmail.com>
2021-04-05 22:15:43 -0300

    feat: create the freight model

---
+++ src/domain/models/freight-model.ts
@@ -0,0 +1,14 @@
+export interface FreightModel {
+  id: string
+  origin_city: string
+  origin_state: string
+  formated_price: string
+  price: number
+  formated_fee: string
+  fee: number
+  vehicle_type: string
+  bodywork: string
+  load_type: string
+  weight: number
+  comments: string
+}
