6cc67d42592037939a8b45c1753459ac667ca3f5
Elian Batista <elianbatista2000@gmail.com>
2021-04-05 22:08:55 -0300

    chore: vehicle entitie

---
+++ src/domain/entities/vehicle.ts
@@ -0,0 +1,12 @@
+export interface Vehicle {
+  modelo: string
+  marca: string
+  ano: string
+  capacidade_maxima: string
+  categoria_veiculo: string
+  placa: string
+  carroceria: string
+  antt: string
+  seguro: boolean
+  rastreador: boolean
+}
