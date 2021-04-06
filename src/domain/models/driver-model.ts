d3c8682b1ea9d6ebf2c99989cf697eb11bb4dfcb
Elian Batista <elianbatista2000@gmail.com>
2021-04-06 18:31:21 -0300

    refactor: add the Freight in DriverModel

--- src/domain/models/driver-model.ts
+++ src/domain/models/driver-model.ts
@@ -1,7 +1,9 @@
+import { FreightModel } from './freight-model'
 import { VehicleModel } from './vehicle-model'
 
 export interface DriverModel {
   id: string
   vehicle: VehicleModel
+  freight: FreightModel[]
   cnh: string
 }
