cff17d5649d2f5d2a49e55acfe179e93c368654d
Elian Batista <elianbatista2000@gmail.com>
2021-04-05 22:15:13 -0300

    refactor: remove the user and freight from company

--- src/domain/entities/company.ts
+++ src/domain/entities/company.ts
@@ -1,11 +1,7 @@
 import { Address } from './address'
-import { Freight } from './freight'
-import { User } from './user'
 
 export interface Company {
-  user: User
-  freight: Freight[]
-  address: Address
+  address: Address[]
   name: string
   cnpj: string
   departament: string
