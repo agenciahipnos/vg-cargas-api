0330f9abfc78a2963550ede1ea79058744dea976
Elian Batista <elianbatista2000@gmail.com>
2021-04-07 21:52:45 -0300

    feat: create the freight mongoose schema

---
+++ src/infra/db/mongodb/schemas/freight-schema.ts
@@ -0,0 +1,57 @@
+import { FreightModel } from '@/domain/models/freight-model'
+import { model, Schema, Model, Document } from 'mongoose'
+
+interface IFreight extends Document, Omit<FreightModel, 'id'> {}
+
+const FreightSchema: Schema = new Schema({
+  available: {
+    type: Boolean,
+    default: true
+  },
+  origin_city: {
+    type: String,
+    required: true
+  },
+  origin_state: {
+    type: String,
+    required: true
+  },
+  formated_price: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  formated_fee: {
+    type: String,
+    required: true
+  },
+  fee: {
+    type: Number,
+    required: true
+  },
+  vehicle_type: {
+    type: String,
+    required: true
+  },
+  bodywork: {
+    type: String,
+    required: true
+  },
+  load_type: {
+    type: String,
+    required: true
+  },
+  weight: {
+    type: Number,
+    required: true
+  },
+  comments: {
+    type: String,
+    required: true
+  }
+})
+
+export const Freight: Model<IFreight> = model('Freight', FreightSchema)
