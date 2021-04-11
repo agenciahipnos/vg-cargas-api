ade1bb8c13dbe9d9ecbce33cbf3b7e28fca506c2
Elian Batista <elianbatista2000@gmail.com>
2021-04-11 19:52:17 -0300

    refactor: change the return of the CreateUserRepository

--- src/domain/usecases/user/create-user-repository.ts
+++ src/domain/usecases/user/create-user-repository.ts
@@ -1,6 +1,11 @@
 import { User } from '@/domain/entities/user'
 import { UserModel } from '@/domain/models/user-model'
 
+export type CreateUserReturnParams = {
+  user: UserModel
+  access_token: string
+}
+
 export interface CreateUserRepository {
-  create: (user: User) => Promise<UserModel>
+  create: (user: User) => Promise<CreateUserReturnParams>
 }
