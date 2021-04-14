63365a8a91104b5dfb70851159890687e3d7e885
Elian Batista <elianbatista2000@gmail.com>
2021-04-14 18:29:53 -0300

    feat: ensure ListUserController returns 500 if ListUserRepository throws

--- src/presentation/controllers/user/list-user/list-user-controller.ts
+++ src/presentation/controllers/user/list-user/list-user-controller.ts
@@ -1,5 +1,5 @@
 import { ListUserRepository } from '@/domain/usecases/user/list-user-repository'
-import { ok } from '@/presentation/helpers/http-helper'
+import { ok, serverError } from '@/presentation/helpers/http-helper'
 import { Controller } from '@/presentation/protocols/controller'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 
@@ -9,8 +9,13 @@ export class ListUserController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const skip = parseInt(httpRequest.query.skip) || 0
-    const users = await this.listUser.list(skip)
-    return ok(users)
+    try {
+      const skip = parseInt(httpRequest.query.skip) || 0
+      const users = await this.listUser.list(skip)
+      return ok(users)
+    } catch (error) {
+      console.error(error)
+      return serverError(error)
+    }
   }
 }
