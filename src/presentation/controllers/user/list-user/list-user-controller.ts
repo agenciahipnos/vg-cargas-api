989469411e09146f23f2b7bdee6e270bd6eb9d0f
Elian Batista <elianbatista2000@gmail.com>
2021-04-14 18:18:33 -0300

    feat: ensure ListUserController calls ListUserRepository with correct values

---
+++ src/presentation/controllers/user/list-user/list-user-controller.ts
@@ -0,0 +1,15 @@
+import { ListUserRepository } from '@/domain/usecases/user/list-user-repository'
+import { Controller } from '@/presentation/protocols/controller'
+import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
+
+export class ListUserController implements Controller {
+  constructor (
+    private readonly listUser: ListUserRepository
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const skip = httpRequest.query.skip
+    await this.listUser.list(skip)
+    return Promise.resolve(null)
+  }
+}
