f0ab5085dd3e10900a3ff58894fc697686968c64
Elian Batista <elianbatista2000@gmail.com>
2021-04-26 21:05:57 -0300

    refactor: add freight user repository

--- src/domain/usecases/user/add-freight-user-repository.ts
+++ src/domain/usecases/user/add-freight-user-repository.ts
@@ -2,5 +2,6 @@ import { FreightModel } from '@/domain/models/freight-model'
 import { UserModel } from '@/domain/models/user-model'
 
 export interface AddFreightUserRepository {
+
   addFreight: (freight: FreightModel, user: UserModel) => Promise<UserModel>
 }
