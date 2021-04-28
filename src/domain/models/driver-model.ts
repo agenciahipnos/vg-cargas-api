4af3bd33f3fdb40ff7d956f7a9fa96f4f8cae766
Elian Batista <elianbatista2000@gmail.com>
2021-04-27 21:47:19 -0300

    refactor: driver model

--- src/domain/models/driver-model.ts
+++ src/domain/models/driver-model.ts
@@ -2,7 +2,7 @@ import { FreightModel } from './freight-model'
 import { VehicleModel } from './vehicle-model'
 
 export interface DriverModel {
-  _id: string
+  _id?: string
   vehicle: VehicleModel
   freight: FreightModel[]
   cnh: string
