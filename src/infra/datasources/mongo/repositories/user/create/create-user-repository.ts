05f97e5220482b2f25f77b1db9f33669112d7446
Elian Batista <elianbatista2000@gmail.com>
2021-04-27 21:47:58 -0300

    feat: create user repository

--- src/infra/datasources/mongo/repositories/user/create/create-user-repository.ts
+++ src/infra/datasources/mongo/repositories/user/create/create-user-repository.ts
@@ -1,9 +1,20 @@
+import jwt from 'jsonwebtoken'
 import { User } from '@/domain/entities/user'
-import { UserModel } from '@/domain/models/user-model'
 import { CreateUserRepository, CreateUserReturnParams } from '@/domain/usecases/user/create-user-repository'
+import { UserMongo } from '@/infra/db/mongodb/schemas/user-schema'
+import env from '@/main/config/env'
 
 export class CreateUserRepositoryImpl implements CreateUserRepository {
   async create (user: User): Promise<CreateUserReturnParams> {
-
+    const create_user = await UserMongo.create(user)
+    if (create_user) {
+      const token = jwt.sign({ id: create_user._id }, env.jwt_secret, {
+        expiresIn: env.jwt_expire_time
+      })
+      return {
+        user: create_user,
+        access_token: token
+      }
+    }
   }
 }
