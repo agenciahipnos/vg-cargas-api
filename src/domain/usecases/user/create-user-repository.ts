6011da12b6aafc95833a1142e1168499c44395c1
Elian Batista <elianbatista2000@gmail.com>
2021-04-28 21:45:15 -0300

    feat: create yser repository

--- src/domain/usecases/user/create-user-repository.ts
+++ src/domain/usecases/user/create-user-repository.ts
@@ -1,11 +1,6 @@
 import { User } from '@/domain/entities/user'
 import { UserModel } from '@/domain/models/user-model'
 
-export type CreateUserReturnParams = {
-  user: UserModel
-  access_token: string
-}
-
 export interface CreateUserRepository {
-  create: (user: User) => Promise<CreateUserReturnParams>
+  create: (user: User) => Promise<UserModel>
 }
