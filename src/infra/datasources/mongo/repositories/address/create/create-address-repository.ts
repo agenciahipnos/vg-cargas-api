79872e92b4d795dd46d3ce5f508fce97c94fdcaa
Elian Batista <elianbatista2000@gmail.com>
2021-04-28 21:46:18 -0300

    feat: create address repository

--- src/infra/datasources/mongo/repositories/address/create/create-address-repository.ts
+++ src/infra/datasources/mongo/repositories/address/create/create-address-repository.ts
@@ -1,9 +1,13 @@
-import { Address } from "@/domain/entities/address";
-import { AddressModel } from "@/domain/models/address-model";
-import { CreateAddressRepository } from "@/domain/usecases/address/create-address-repository";
+import { Address } from '@/domain/entities/address'
+import { AddressModel } from '@/domain/models/address-model'
+import { CreateAddressRepository } from '@/domain/usecases/address/create-address-repository'
+import { AddressMongo } from '@/infra/db/mongodb/schemas/address-schema'
 
 export class CreateAddressRepositoryImpl implements CreateAddressRepository {
   async create (address: Address): Promise<AddressModel> {
-    const create_address = await 
+    const create_address = await AddressMongo.create(address)
+    if (create_address) {
+      return create_address
+    }
   }
 }
