b56b8d5e432b15782882c3665da12a640659a54a
Elian Batista <elianbatista2000@gmail.com>
2021-04-30 18:10:44 -0300

    feat: create the create user controller factory

---
+++ src/main/factories/controllers/user/create-user-controller-factory.ts
@@ -0,0 +1,25 @@
+import { RSAAdapter } from '@/infra/cryptography/rsa-adapter'
+import { CreateAddressRepositoryImpl } from '@/infra/datasources/mongo/repositories/address/create/create-address-repository'
+import { CreateCompanyRepositoryImpl } from '@/infra/datasources/mongo/repositories/company/create/create-company-repository'
+import { CreateDriverRepositoryImpl } from '@/infra/datasources/mongo/repositories/driver/create/create-driver-repository'
+import { CreateUserRepositoryImpl } from '@/infra/datasources/mongo/repositories/user/create/create-user-repository'
+import { CreateUserController } from '@/presentation/controllers/user/create-user/create-user-controller'
+import { ValidatorClass } from '@/presentation/helpers/validator'
+import { Controller } from '@/presentation/protocols/controller'
+import { makeDbCreateUser } from '../../usecases/user/db-create-user-factory'
+
+export const makeCreateUserController = (): Controller => {
+  const validator = new ValidatorClass()
+  const decrypter = new RSAAdapter()
+  const create_address_repository = new CreateAddressRepositoryImpl()
+  const create_company_repository = new CreateCompanyRepositoryImpl()
+  const create_driver_repository = new CreateDriverRepositoryImpl()
+  return new CreateUserController(
+    decrypter,
+    validator,
+    makeDbCreateUser(),
+    create_address_repository,
+    create_driver_repository,
+    create_company_repository
+  )
+}
