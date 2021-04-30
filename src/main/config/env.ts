c5a4bc983341b6e8fca47468b200c88b8badd704
Elian Batista <elianbatista2000@gmail.com>
2021-04-30 18:10:33 -0300

    refactor: update the env.ts

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,5 +4,9 @@ dotenv.config()
 
 export default {
   mongoUrl: process.env.MONGO_URL || '',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwt_secret: process.env.JWT_SECRET || '',
+  jwt_expire_time: process.env.JWT_EXPIRE_TIME || '',
+  public_key: process.env.PUBLIC_KEY || '',
+  private_key: process.env.PRIVATE_KEY || ''
 }
