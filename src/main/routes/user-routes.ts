03d5375e57ad571a8e81d6f80c476cca2e3e4815
Elian Batista <elianbatista2000@gmail.com>
2021-05-02 21:47:21 -0300

    feat: create the user route

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-adapter-route'
+import { makeCreateUserController } from '../factories/controllers/user/create-user-controller-factory'
 
 export default (router: Router): void => {
-  router.post('/usuario', adaptRoute())
+  router.post('/user', adaptRoute(makeCreateUserController()))
 }
