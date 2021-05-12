98eb6d2b6da4df50c5cea2f5ec9f7d64abbfce01
Elian Batista <elianbatista2000@gmail.com>
2021-05-11 21:26:22 -0300

    company

--- src/domain/entities/company.ts
+++ src/domain/entities/company.ts
@@ -1,9 +1,7 @@
 import { Address } from './address'
-import { Freight } from './freight'
 
 export interface Company {
   address: Address[]
-  freight: Freight[]
   name: string
   cnpj: string
   departament: string
