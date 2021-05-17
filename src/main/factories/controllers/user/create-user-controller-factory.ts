ae08a93830e0b2761b4e297e53ceb4b24afd13fd
Elian Batista <elianbatista2000@gmail.com>
2021-05-17 16:13:46 -0300

    create user controller factory

--- src/main/factories/controllers/user/create-user-controller-factory.ts
+++ src/main/factories/controllers/user/create-user-controller-factory.ts
@@ -2,6 +2,7 @@ import { RSAAdapter } from '@/infra/cryptography/rsa-adapter'
 import { CreateAddressRepositoryImpl } from '@/infra/datasources/mongo/repositories/address/create/create-address-repository'
 import { CreateCompanyRepositoryImpl } from '@/infra/datasources/mongo/repositories/company/create/create-company-repository'
 import { CreateDriverRepositoryImpl } from '@/infra/datasources/mongo/repositories/driver/create/create-driver-repository'
+import { FindUserRepositoryImpl } from '@/infra/datasources/mongo/repositories/user/find/find-user-repository'
 import { CreateUserController } from '@/presentation/controllers/user/create-user/create-user-controller'
 import { ValidatorClass } from '@/presentation/helpers/validator'
 import { Controller } from '@/presentation/protocols/controller'
@@ -13,12 +14,14 @@ export const makeCreateUserController = (): Controller => {
   const create_address_repository = new CreateAddressRepositoryImpl()
   const create_company_repository = new CreateCompanyRepositoryImpl()
   const create_driver_repository = new CreateDriverRepositoryImpl()
+  const find_user_repository = new FindUserRepositoryImpl()
   return new CreateUserController(
     decrypter,
     validator,
     makeDbCreateUser(),
     create_address_repository,
     create_driver_repository,
-    create_company_repository
+    create_company_repository,
+    find_user_repository
   )
 }
